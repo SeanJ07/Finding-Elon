3528ebe5794bada9c4d1cb8ea3c40aa49d395bea
Viramontes <735179@student.dusd.net>
2022-05-25 13:36:55 -0700

    y
    y

--- FindingElonFinal/Assets/Scripts/PlayerController.cs
+++ FindingElonFinal/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour
 
     private AudioSource playerAudio;
     public AudioClip jumpSound;
+    public bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +38,14 @@ public class PlayerController : MonoBehaviour
         Vector2 position = rb.position;
 
         moveInput = Input.GetAxisRaw("Horizontal");
+        if (moveInput > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveInput< 0 && facingRight)
+        {
+            Flip();
+        }
         animator.SetFloat("Speed", Mathf.Abs(moveInput));
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
@@ -51,10 +60,12 @@ public class PlayerController : MonoBehaviour
             jumpTimeCounter = jumpTime;
             rb.velocity = Vector2.up * jumpForce;
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+            animator.SetBool ("IsJumping", true);
+           
         }
         if (Input.GetKey(KeyCode.Space) && isJumping == true)
         {
-            if(jumpTimeCounter > 0)
+            if(jumpTimeCounter > .01)
             {
               rb.velocity = Vector2.up * jumpForce;
                 jumpTimeCounter -= Time.deltaTime;
@@ -62,11 +73,25 @@ public class PlayerController : MonoBehaviour
             else
             {
                 isJumping = false;
+                
             }
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isJumping = false; 
         }
+        if (isGrounded == false && jumpTimeCounter !> .01 && isJumping == false)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("isFalling", true);
+        }
+    }
+
+     void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
